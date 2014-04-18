965fbbc2b4da3a852b0c654b253c12fb9e3aeff0
Roger Garcia <roger@expertte.com>
2014-04-18 15:34:47 -0300

    added collision (needs BUGFIX)

--- js.js
+++ js.js
@@ -11,9 +11,77 @@ function tickControls() {
 }
 
 function tickPositions() {
-  for(i in Game.movableElements) {
+  for(var i in Game.movableElements) {
     var e = Game.movableElements[i];
     e.tickPosition();
+  }
+}
+
+function elasticCollision(a, b) {
+  var normal = new Vector(a.x - b.x, a.y - b.y).normalize(),
+  tangent = normal.getPerpendicular(),
+  av  = new Vector(a.vx, a.vy),
+  bv  = new Vector(b.vx, b.vy),
+  avn = normal.dotProduct(av),  // decompose normal component
+  bvn = normal.dotProduct(bv),
+  avt = tangent.dotProduct(av), // decompose tangent component
+  bvt = tangent.dotProduct(bv),
+  am  = a.mass,
+  bm  = b.mass,
+  avnAfter = bvnAfter = avtAfter = bvtAfter = null, // scalars
+  avAfter = bvAfter = null;                         // vectors
+
+  // Game.debug('normal: (' + normal.x + ', ' + normal.y + ')');
+  // Game.debug('tangent: (' + tangent.x + ', ' + tangent.y + ')');
+
+  if(am + bm == 0) {
+    Game.debug('ERROR: sum of object masses must not be zero');
+    return;
+  }
+
+  // calculate collision on normal components
+  avnAfter = avn * (am - bm) + 2 * bm * bvn;
+  avnAfter /= (am + bm);
+
+  bvnAfter = bvn * (bm - am) + 2 * am * avn;
+  bvnAfter /= (am + bm);
+
+  // tangent components stay the same
+  avtAfter = avt;
+  bvtAfter = bvt;
+
+  // make resulting vectors
+  avAfter = normal.getScaled(avnAfter).add(tangent.getScaled(avtAfter));
+  bvAfter = normal.getScaled(bvnAfter).add(tangent.getScaled(bvtAfter));
+
+  // return new velocities
+  return {
+    av: avAfter,
+    bv: bvAfter
+  };
+}
+
+function detectCollisions() {
+  var elementCount = Game.movableElements.length;
+
+  for(var i = 0; i < elementCount; ++i) {
+    var e1 = Game.movableElements[i];
+
+    for(var j = i + 1; j < elementCount; ++j) {
+      var e2 = Game.movableElements[j],
+      centerDist = new Vector(e1.x - e2.x, e1.y - e2.y),
+      radiusSum = e1.radius + e2.radius;
+
+      if(centerDist.getMagnitude() <= radiusSum){
+        Game.debug('collision between objects ' + i + ' and ' + j);
+        var velAfter = elasticCollision(e1, e2);
+
+        e1.vx = velAfter.av.x;
+        e1.vy = velAfter.av.y;
+        e2.vx = velAfter.bv.x;
+        e2.vy = velAfter.bv.y;
+      }
+    }
   }
 }
 
@@ -21,11 +89,14 @@ function handleTick() {
   // handle key presses
   tickControls();
 
+  // wrap movable elements in toroid
+  wrapMap();
+
+  detectCollisions();
+
   // handle objects movement
   tickPositions();
 
-  // wrap movable elements in toroid
-  wrapMap();
 
   // debug text
   var debugText = "angle: " + Game.player1.angle;
